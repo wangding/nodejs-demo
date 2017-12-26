47e329ada89811b7dbd64f2d0c6d4cd626082cab
wangding <408542507@qq.com>
2017-12-26 09:11:51 +0800

    add 18-debug demo code

---
+++ 18-debug/profile.js
@@ -0,0 +1,10 @@
+function makeLoad() {
+  for(var i=0; i<100000000000; i++);
+}
+
+function logSomething() {
+  console.log('something');
+}
+
+setInterval(makeLoad, 2000);
+setInterval(logSomething, 0);
