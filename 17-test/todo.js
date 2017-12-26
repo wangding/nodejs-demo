45e315c9530d6ef833900ca8b8b1d57bdc51ff70
wangding <408542507@qq.com>
2017-12-26 09:11:30 +0800

    add 17-test demo code

---
+++ 17-test/todo.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+function Todo() {
+  this.todos = [];
+}
+
+Todo.prototype.add = function(item) {
+  if(!item) throw new Error('Todo#add require an item');
+  this.todos.push(item);
+}
+
+Todo.prototype.deleteAll = function() {
+  this.todos = [];
+}
+
+Todo.prototype.getCount = function() {
+  return this.todos.length;
+}
+
+Todo.prototype.doAsync = function(cb) {
+  setTimeout(cb, 2000, true);
+}
+
+module.exports = Todo;
---
+++ 17-test/todo.test.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+
+var assert = require('assert');
+var Todo = require('./todo');
+
+var todo = new Todo();
+var testCompleted = 0;
+
+deleteTest();
+addTest();
+throwTest();
+doAsyncTest(function() {
+  console.log('colmpleted %d tests.', testCompleted);
+})
+
+function deleteTest() {
+  console.log('test delete function');
+  todo.add('Delete Me');
+  assert.equal(todo.getCount(), 1, '1 item should exist');
+  todo.deleteAll();
+  assert.equal(todo.getCount(), 0, 'No items should exist');
+  testCompleted++;
+}
+
+function addTest() {
+  console.log('test add function');
+  todo.deleteAll();
+  todo.add('Added');
+  assert.notEqual(todo.getCount(), 0, '1 item should exist');
+  assert.equal(todo.getCount(), 1, 'i item should exist');
+  testCompleted++;
+}
+
+function throwTest() {
+  console.log('test add function');
+  assert.throws(todo.add, /require/);
+  testCompleted++;
+}
+
+function doAsyncTest(cb) {
+  console.log('test doAsync function');
+  todo.doAsync(function(value) {
+    assert(value, 'callback should be passed true');
+    testCompleted++;
+    cb();
+  });
+}
