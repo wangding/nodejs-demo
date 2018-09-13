6cc696787451293121ed9f6273c350d4d13a7362
wangding <408542507@qq.com>
2018-09-13 08:36:38 +0800

    modify console demo

--- 03-console/01-format.js
+++ 03-console/01-format.js
@@ -6,10 +6,23 @@ const user = {
   qq:   '408542507'
 };
 
-console.log('name: %s', user.name);
-console.log('qq:', user.qq);
-console.log(`qq: ${user.qq}`);
-console.log('age: %d', user.age);
-console.log('JSON: %j', user);
+const log = console.log;
+
+// 三种占位符
+log('name: %s', user.name);
+log('age: %d', user.age);
+log('JSON: %j', user);
+
+// 输出方式一：占位符输出
+log('qq: %s', user.qq);
+
+// 输出方式二：逗号间隔，多变量输出
+log('qq:', user.qq);
+
+// 输出方式三：拼接字符串输出
+log('qq: ' + user.qq);
+
+// 输出方式四：模板字符串输出
+log(`qq: ${user.qq}`);
 
 console.error('Error! something wrong!');
