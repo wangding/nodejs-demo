ed86c72a32e358d21ecd013c7c11f6e5908b61ef
wangding <408542507@qq.com>
2018-12-18 10:42:35 +0800

    add http-server demo

--- 16-http-server/06-upload-pic/index.js
+++ 16-http-server/06-upload-pic/index.js
@@ -3,12 +3,19 @@
 const http = require('http'),
       fs   = require('fs'),
       qs   = require('querystring'),
-      log  = require('util').debuglog('UPLOAD-FILE');
+      path = require('path'),
+      jsdom = require('jsdom'),
+      { JSDOM } = jsdom,
+      log  = console.log;
 
 http.createServer((req, res) => {
   log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
   log(req.headers);
   log('');
+
+  if(req.method === 'GET' && req.url === 'images') {
+    
+  }
 
   if(req.method === 'POST') {
     if(req.url !== '/upload') {
@@ -28,13 +35,15 @@ http.createServer((req, res) => {
       var files = file.split('\r\n')[1].split(';');
       var fileName = qs.parse(files[2].trim())['filename'];
       fileName = fileName.slice(1, fileName.length-1);
+      fileName = path.join(__dirname, 'images', fileName);
       fs.writeFileSync(fileName, buf, {'encoding': 'binary'});
     });
 
-    show(res, okPage);
+    show(res, renderUploadPg());
+
   } else {
     if(req.url === '/') {
-      show(res, uploadPage);
+      show(res, renderUploadPg());
     } else {
       show(res, errorPage);
     }
@@ -49,34 +58,38 @@ function show(res, page) {
   res.end(page);
 }
 
-var uploadPage = ''
-    + '<!DOCTYPE html>'
-    + '<html>'
-      + '<head>'
-        + '<meta charset="UTF-8">'
-        + '<title>Upload File</title>'
-      + '</head>'
-      + '<body>'
-        + '<h1>Upload File</h1>'
-        + '<form method="post" enctype="multipart/form-data" action="/upload">'
-          + '<input type="file" name="upload">'
-          + '<input type="submit" value="Upload">'
-        + '</form>'
-      + '</body>'
-    + '</html>';
+function renderUploadPg() {
+  var uploadPage = ''
+      + '<!DOCTYPE html>'
+      + '<html>'
+        + '<head>'
+          + '<meta charset="UTF-8">'
+          + '<title>Upload Picture</title>'
+        + '</head>'
+        + '<body>'
+          + '<h1>Upload Picture</h1>'
+          + '<form method="post" enctype="multipart/form-data" action="/upload">'
+            + '<input type="file" name="upload" accept=".png, .jpg, .jpeg, .gif">'
+            + '<input type="submit" value="Upload">'
+          + '</form>'
+          + '<div id="album"></div>'
+        + '</body>'
+      + '</html>';
 
-var okPage = ''
-    + '<!DOCTYPE html>'
-    + '<html>'
-      + '<head>'
-        + '<meta charset="UTF-8">'
-        + '<title>Upload File</title>'
-      + '</head>'
-      + '<body>'
-        + '<h1>Upload File OK!</h1>'
-        + '<a href="/">continue to upload</a>'
-      + '</body>'
-    + '</html>';
+  const dom = new JSDOM(uploadPage);
+  const $   = require('jquery')(dom.window);
+
+  var $album = $('#album'),
+      images = fs.readdirSync('./images');
+
+  images.forEach((img) =>{
+    var $image = $('<img>');
+    $image.attr('src',  'http://192.168.174.144:8080/images/' + img);
+    $album.append($image);
+  });
+
+  return dom.serialize();
+}
 
 var errorPage = ''
     + '<!DOCTYPE html>'
