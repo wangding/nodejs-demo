f5564192a5ed331d3b2e9f1fb90a07b221b046e3
wangding <408542507@qq.com>
2019-08-30 16:40:48 +0800

    modify console demo

--- 03-console/01-format.js
+++ 03-console/01-format.js
@@ -9,20 +9,13 @@ const user = {
 const log = console.log;
 
 // 三种占位符
-log('name: %s', user.name);
-log('age: %d', user.age);
-log('JSON: %j', user);
+log('name: %s', user.name);   // 字符串类型
+log('age: %d', user.age);     // 整数类型
+log('JSON: %j', user);        // 对象类型
 
-// 输出方式一：占位符输出
-log('qq: %s', user.qq);
-
-// 输出方式二：逗号间隔，多变量输出
-log('qq:', user.qq);
-
-// 输出方式三：拼接字符串输出
-log('qq: ' + user.qq);
-
-// 输出方式四：模板字符串输出
-log(`qq: ${user.qq}`);
+log('qq: %s', user.qq);       // 输出方式一：占位符输出
+log('qq:', user.qq);          // 输出方式二：逗号间隔，多变量输出
+log('qq: ' + user.qq);        // 输出方式三：拼接字符串输出
+log(`qq: ${user.qq}`);        // 输出方式四：模板字符串输出
 
 console.error('Error! something wrong!');
