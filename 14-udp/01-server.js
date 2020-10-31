6795a7ca65f6b33fcb3993163b83af10f57bb1cd
wangding <408542507@qq.com>
2020-10-31 17:06:33 +0800

    modify 14-udp

--- 14-udp/01-server.js
+++ 14-udp/01-server.js
@@ -1,12 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const dgram = require('dgram');
 
-var socket =  dgram.createSocket('udp4');
+let socket = dgram.createSocket('udp4');
 socket.bind(8080);
 
 socket.on('message', (msg) => {
-  var line = msg.toString('utf8');
+  let line = msg.toString('utf8');
 
   process.stdout.write(line, line.length);
 });
--- 14-udp/02-my-nc.js
+++ 14-udp/02-my-nc.js
@@ -1,12 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const dgram  = require('dgram'),
       socket = dgram.createSocket('udp4');
 
-var host = process.argv[2];
-var port = process.argv[3];
+let host = process.argv[2],
+    port = process.argv[3];
 
 process.stdin.on('data', (data) => {
-  var line = data.toString('utf8');
+  let line = data.toString('utf8');
   socket.send(line, 0, line.length, port, host);
 });
