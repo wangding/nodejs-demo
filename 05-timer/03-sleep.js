79c1d98a67f671912146d4721d17d1c914542a8a
wangding <408542507@qq.com>
2022-09-12 22:08:15 +0800

    add 03-sleep.js

---
+++ 05-timer/03-sleep.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+//const { setTimeout } = require('node:timers/promises');
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve();
+    }, ms);
+  })
+}
+
+async function main() {
+  console.log('hello');
+  await sleep(2000);
+  //await setTimeout(1000);
+  console.log('world');
+}
+
+main();
